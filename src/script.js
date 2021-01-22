6d91c5e72ea1e9d32dc2416c4ad6956501bf82cf
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2021-01-21 19:26:04 -0500

    stuff

--- src/script.js
+++ src/script.js
@@ -1,20 +1,20 @@
-if (process.platform != "darwin") {
-  alert("Sorry, this app only supports macOS.")
+if (process.platform !== 'darwin') {
+  alert('Sorry, this app only supports macOS.')
   /* const { remote } = require('electron')
   let w = remote.getCurrentWindow()
   w.close() */
-  const { remote } = require("electron")
+  const { remote } = require('electron')
   remote.getCurrentWindow().close()
 }
-const { exec } = require("child_process")
+const { exec } = require('child_process')
 exec('ioreg -abrc AppleSmartBattery | plist-to-json /dev/stdin', (err, stdout) => {
   if (err) {
     alert(err)
   } else {
     stdout = JSON.parse(stdout)[0][0]
     Object.keys(stdout).forEach((key) => {
-      let data = typeof stdout[key] == "string" ? stdout[key] : JSON.stringify(stdout[key])
-      document.write(`<b>${key}</b> ${data}<br/>`)
+      const data = typeof stdout[key] === 'string' ? stdout[key] : JSON.stringify(stdout[key])
+      document.getElementById('info').innerHTML += `<b>${key}</b> ${data}<br/>`
     })
     console.log(stdout)
   }
