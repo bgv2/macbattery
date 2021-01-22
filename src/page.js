a4069679194b58071d4881e6966fde52d8965870
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2021-01-21 19:15:37 -0500

    initial

---
+++ src/page.js
@@ -0,0 +1,21 @@
+if (process.platform != "darwin") {
+  alert("Sorry, this app only supports macOS.")
+  /* const { remote } = require('electron')
+  let w = remote.getCurrentWindow()
+  w.close() */
+  const { remote } = require("electron")
+  remote.getCurrentWindow().close()
+}
+const { exec } = require("child_process")
+exec('ioreg -abrc AppleSmartBattery | plist-to-json /dev/stdin', (err, stdout) => {
+  if (err) {
+    alert(err)
+  } else {
+    stdout = JSON.parse(stdout)[0][0]
+    Object.keys(stdout).forEach((key) => {
+      let data = typeof stdout[key] == "string" ? stdout[key] : JSON.stringify(stdout[key])
+      document.write(`<b>${key}</b> ${data}<br/>`)
+    })
+    console.log(stdout)
+  }
+})
