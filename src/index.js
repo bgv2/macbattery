c357ce2b34f4a3b987a132c4841bb7a5d666850d
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2023-08-07 12:58:23 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,9 +29,7 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed, even on macOS.
 app.on('window-all-closed', () => {
   // if (process.platform !== 'darwin') {
   app.quit()
